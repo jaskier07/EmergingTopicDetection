fb4c35541f9169696109bf4831720fca2f08b6aa
jaskier07 <aleksanderkania96@gmail.com>
2020-07-22 09:51:59 +0200

    Spring starter project

---
+++ src/test/java/pl/kania/petd/PersonalizedEmergingTopicDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kania.petd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalizedEmergingTopicDetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
