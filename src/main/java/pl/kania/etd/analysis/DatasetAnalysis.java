d9fdb62c6224c7f4ca880383d955b3fcbeb8728c
jaskier07 <aleksanderkania96@gmail.com>
2020-09-22 23:05:44 +0200

    Refactor and javadoc

--- src/main/java/pl/kania/etd/analysis/DatasetAnalysis.java
+++ src/main/java/pl/kania/etd/analysis/DatasetAnalysis.java
@@ -15,7 +15,16 @@ import pl.kania.etd.periods.TimePeriodGenerator;
 import pl.kania.etd.periods.TimePeriodInTweetsSetter;
 
 import java.util.List;
+import java.util.Set;
 
+/**
+ * App printing statistics about a dataset.
+ *
+ * Results are saved in multiple files.
+ *
+ * App is memory consuming. Run it with -Xmx8192m JVM parameter while using provided coronavirus
+ * tweet dataset.
+ */
 @Slf4j
 @SpringBootApplication
 public class DatasetAnalysis {
@@ -34,6 +43,11 @@ public class DatasetAnalysis {
         TimePeriodInTweetsSetter.setTimePeriod(periods, csvReaderResult.getTweetSet());
         MemoryService.printCurrentFreeMemory();
 
+        new TweetsInPeriodCounter(fop, periods).write();
         new AnalyseWordUsage(fop).analyse(periods);
+        new AnalyseAuthorsTweets(fop, periods, Set.of("realDonaldTrump", "CNN", "WhiteHouse", "ArvindKejriwal", "premierleague"));
+        new MostPopularAuthors(fop).write();
+        new MostPopularWords(fop, periods).write();
+        new StatisticsInDataset(fop, periods).write();
     }
 }
